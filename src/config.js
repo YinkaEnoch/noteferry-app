f16216bdc9d3f23e3f8e5c5231fc0e379f57ba07
yinkaenoch <adedokunyinkaenoch@gmail.com>
2022-05-06 13:20:01 +0100

    Chore: delete entire codebase

--- src/config.js
+++
@@ -1,7 +0,0 @@
-let API_URL = "";
-
-process.env.NODE_ENV === "development"
-  ? (API_URL = "/noteferry/note/")
-  : (API_URL = "https://noteferry.herokuapp.com/noteferry/note/");
-
-export { API_URL };
